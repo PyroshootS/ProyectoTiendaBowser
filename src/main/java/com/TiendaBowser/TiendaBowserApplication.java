92b28214c9ab1ee3fd731361e7571233b3a45d28
Pyro <oreinavallecillo@gmail.com>
2023-07-24 13:30:24 -0600

    Primera Prueba

---
+++ src/main/java/com/TiendaBowser/TiendaBowserApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaBowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaBowser/TiendaBowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaBowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaBowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
