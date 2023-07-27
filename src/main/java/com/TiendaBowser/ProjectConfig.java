2d8168a0504757af794a4e5f369885a034f0f728
Pyro <oreinavallecillo@gmail.com>
2023-07-27 13:18:34 -0600

    New Branch Respaldo donde se hace todos los cambios de colores

--- src/main/java/com/TiendaBowser/ProjectConfig.java
+++ src/main/java/com/TiendaBowser/ProjectConfig.java
@@ -73,11 +73,11 @@ public class ProjectConfig implements WebMvcConfigurer {
         http
                 .authorizeHttpRequests((request) -> request
                 .requestMatchers("/", "/index", "/errores/**",
-                        "/carrito/**",
+                        "/carrito/**","/juegos/listado",
                         "/registro/**", "/js/**", "/webjars/**")
                 .permitAll()
                 .requestMatchers(
-                        "/juego/nuevo", "/juego/guardar",
+                        "/juego/nuevo", "/juego/guardar","/juegos/listado",
                         "/juego/modificar/**", "/juego/eliminar/**",
                         "/categoria/nuevo", "/categoria/guardar",
                         "/categoria/modificar/**", "/categoria/eliminar/**",
@@ -99,27 +99,7 @@ public class ProjectConfig implements WebMvcConfigurer {
         return http.build();
     }
 
-//    /* El siguiente método se utiliza para completar la clase no es 
-//    realmente funcional, la próxima semana se reemplaza con usuarios de BD */
-//    @Bean
-//    public UserDetailsService users() {
-//        UserDetails admin = User.builder()
-//                .username("juan")
-//                .password("{noop}123")
-//                .roles("USER", "VENDEDOR", "ADMIN")
-//                .build();
-//        UserDetails sales = User.builder()
-//                .username("rebeca")
-//                .password("{noop}456")
-//                .roles("USER", "VENDEDOR")
-//                .build();
-//        UserDetails user = User.builder()
-//                .username("pedro")
-//                .password("{noop}789")
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, sales, admin);
-//    }
+
     
     
     @Autowired UserDetailsService userDetailsService;
